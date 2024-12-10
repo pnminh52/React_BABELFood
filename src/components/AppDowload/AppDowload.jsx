0e945dc1d700f9959d70d411825801c0c38afa40
minh-eng <phamnhatminhsuzumoe52@gmail.com>
2024-12-10 20:00:07 +0700

    Create App Dowload Components

--- src/components/AppDowload/AppDowload.jsx
+++ src/components/AppDowload/AppDowload.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./AppDowload.css";
+import { assets } from "../../assets/assets";
 const AppDowload = () => {
-  return <div></div>;
+  return (
+    <div className="app-dowload" id="app-dowload">
+      <p>
+        For Better Experience Dowload <br />
+        Tomato App
+      </p>
+      <div className="app-dowload-platform">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default AppDowload;
